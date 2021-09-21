24423cd4e7305a6c5bc4b276f44e357f0d522b4e
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-09-21 10:42:42 -0500

    ...

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { api } = require("./api");
 const PORT = process.env.PORT || 8080;
 
+console.log("PORT to listen", PORT);
+
 api.listen(PORT, () => console.log(`graphviz-web listening on port ${PORT}`));
