09c7b00408d446a5675dffd32347647c9a7f8392
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-09-21 11:22:23 -0500

    ....config dev

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
 const configJson = require("./config");
 const url = require("url");
+const serviceAccountEventsDev = require("./ebombo-events-dev-firebase-adminsdk-brntw-ad09602471.json");
 
 const config =
   process.env.NODE_ENV === "production"
@@ -9,7 +10,10 @@ const config =
 
 process.env.NODE_ENV === "production"
   ? admin.initializeApp()
-  : admin.initializeApp(config.firebase);
+  : admin.initializeApp({
+      credential: admin.credential.cert(serviceAccountEventsDev),
+      databaseURL: config.firebase.databaseURL,
+    });
 
 const adminFirestore = admin.firestore;
 const firestore = admin.firestore();
