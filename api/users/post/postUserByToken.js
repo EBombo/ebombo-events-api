daa27c2dd89c42c911b60f0a2d2290b585d9b032
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-10-20 16:37:50 -0500

    refactoring validate authUser

--- api/users/post/postUserByToken.js
+++ api/users/post/postUserByToken.js
@@ -6,17 +6,22 @@ exports.postUserByToken = async (req, res, next) => {
   try {
     logger.log("postUserByToken", req.body);
 
-    const { tokenId } = req.body;
+    let { tokenId, userId } = req.body;
 
-    const authUser = await auth.verifyIdToken(tokenId);
+    if (!tokenId && !userId) throw Error("token o userId is required!");
 
-    const _user = await fetchUser(authUser.uid);
+    if (tokenId) {
+      const authUser = await auth.verifyIdToken(tokenId);
+      userId = authUser.uid;
+    }
+
+    const _user = await fetchUser(userId);
 
     return res.send({
       user: {
         name: _user.name,
         email: _user.email,
-        uid: authUser.uid,
+        uid: userId,
       },
     });
   } catch (error) {
