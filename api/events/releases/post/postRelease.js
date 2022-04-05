6d6fde2c610c33ff828e09b2ab65ea4c62840010
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2022-04-05 09:59:08 -0500

    adding delete variable

--- api/events/releases/post/postRelease.js
+++ api/events/releases/post/postRelease.js
@@ -14,6 +14,7 @@ exports.postRelease = async (req, res, next) => {
 
     await updateRelease(eventId, releaseId, {
       ...release,
+      deleted: false,
       createAt: new Date(),
       updateAt: new Date(),
     });
