816d00ff35060995f294c418c62b2a20b79518a1
Pablo <pablo.yncio@immertec.com>
2022-06-16 12:49:13 -0500

    create collections for contacts

--- api/contact/post/businessEmail.js
+++ api/contact/post/businessEmail.js
@@ -1,22 +1,38 @@
-const { config } = require("../../../config");
+const { config, firestore } = require("../../../config");
 const { sendEmail } = require("../../../email/sendEmail");
 const { fetchTemplates } = require("../../../collections/settings");
 
 const businessEmail = async (req, res, next) => {
   try {
-    let message = req.body.message;
-    const phoneNumber = req.body.phoneNumber;
     const email = req.body.email;
+    let message = req.body.message;
+    const isBdev = req.body.isBdev;
+    const phoneNumber = req.body.phoneNumber;
     let interests = "Servicios de interes: " + req.body.interests.join(",");
 
     const templates = await fetchTemplates();
     const templateBusiness = templates["business"];
 
-    await sendEmail_(config.mails, message + interests, phoneNumber, email, templateBusiness);
+    const promiseEmail = sendEmail_(config.mails, message + interests, phoneNumber, email, templateBusiness);
+
+    const contactsRef = firestore.collection("contacts");
+    const contactId = contactsRef.doc().id;
+    const promiseFirebase = firestore.doc(`contacts/${contactId}`).set({
+      id: contactId,
+      email,
+      isBdev,
+      message,
+      interests,
+      phoneNumber,
+      deleted: false,
+      createAt: new Date(),
+      updateAt: new Date(),
+    });
+
+    await Promise.all([promiseEmail, promiseFirebase]);
 
     return res.send(200);
   } catch (error) {
-    console.error("Post message chat", error);
     next(error);
   }
 };
