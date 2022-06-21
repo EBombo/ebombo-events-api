ac57768cdc4b8c4cc2ab452fc7ab435127376f6b
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2022-06-21 18:09:12 -0500

    refactoring put event

--- api/events/put/putEvent.js
+++ api/events/put/putEvent.js
@@ -1,6 +1,6 @@
 const { firestore, config } = require("../../../config");
 const logger = require("../../../utils/logger");
-const { fetchTemplates } = require("../../../collections/settings");
+const { fetchTemplates, fetchTemplate } = require("../../../collections/settings");
 const { sendEmail } = require("../../../email/sendEmail");
 const { updateEvent } = require("../../../collections/events");
 const moment = require("moment");
@@ -14,16 +14,18 @@ const putEvent = async (req, res, next) => {
 
     const members = event.members ?? [];
 
-    const startDateFormatted = `${event.currentDate.month.format("DD/MM/YYYY")} ${event.currentDate.start.format(
-      "h:mm a"
-    )}`;
+    const startDateFormatted = `${moment(event.currentDate.month).format("DD/MM/YYYY")} ${moment(
+      event.currentDate.start
+    ).format("h:mm a")}`;
 
-    const endDateFormatted = `${event.currentDate.month.format("DD/MM/YYYY")} ${event.currentDate.end.format(
-      "h:mm a"
-    )}`;
+    const endDateFormatted = `${moment(event.currentDate.month).format("DD/MM/YYYY")} ${moment(
+      event.currentDate.end
+    ).format("h:mm a")}`;
 
     const startAt = moment(startDateFormatted, "DD/MM/YYYY h:mm a").toDate();
     const endAt = moment(endDateFormatted, "DD/MM/YYYY h:mm a").toDate();
+
+    delete event.currentDate;
 
     await updateEvent(eventId, { ...event, startAt, endAt, updateAt: new Date() });
 
@@ -37,8 +39,7 @@ const putEvent = async (req, res, next) => {
 
 const sendEmailToMembers = async (event, members) => {
   try {
-    const templates = await fetchTemplates();
-    const eventTemplate = templates["eventTemplate"];
+    const eventTemplate = await fetchTemplate("eventTemplate");
 
     const promisesEmails = members.map(async (member) => await sendEmailToUsers(member, event, eventTemplate));
 
@@ -49,10 +50,12 @@ const sendEmailToMembers = async (event, members) => {
 };
 
 const sendEmailToUsers = async (user, event, template) =>
-  await sendEmail(user.email.trim(), event.name, template.content, {
-    startAt: event.startAt,
-    endAt: event.endAt,
+  await sendEmail(user.email.trim(), event.name, template, {
+    startAt: moment(event.startAt).format("DD/MM/YYYY h:mm a"),
+    link: event.link,
     userEmail: user.email.trim(),
+    imageUrl: event.imageUrl,
+    eventName: event.name,
   });
 
 module.exports = { putEvent };
